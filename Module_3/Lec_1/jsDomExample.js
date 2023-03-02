53d97ad5a3db3677ddbb329402d18496dd14377b
the-luffy <abhisheklaxkar007@gmail.com>
2023-03-02 17:45:12 +0530

    extract file with jsdom

--- Module_3/Lec_1/jsDomExample.js
+++ Module_3/Lec_1/jsDomExample.js
@@ -1,13 +1,27 @@
 // npm i jsdom
-const jsdom = require('jsdom');
 const fs = require('fs');
-const JSDOM = jsdom.JSDOM;
-
 let htmlContent =fs.readFileSync("sample.html", "utf-8");
+
+const jsdom = require('jsdom');
+const JSDOM = jsdom.JSDOM;//no meaning
+//1. string form me -> html content provide
+// pass to newJSDOM
 let dom = new JSDOM(htmlContent);
+// 2. no meaning
 let document = dom.window.document;
 
-
-
-//3. logic -> help to get your html element if you pass a selector
-document.querySelector();
+//3. logic -> help to get your html element 
+//if you pass a selector
+//css selector -> first occurence of that html element
+// document.querySelector("p");
+//css selector -> all the occurence of that html element
+// const allPs = document.querySelectorAll("p");
+// console.log(allPs.length);
+// // content ->
+let firstPkacontent = allPs[0].textContent;
+console.log("first P ka content",firstPkacontent);
+let secondPkacontent = allPs[1].textContent;
+console.log("first P ka content",secondPkacontent);
+const allidWalaElem = document.querySelectorAll("#unique");
+let content = allidWalaElem[0].textContent;
+console.log(content);
--- Module_3/Lec_1/req.js
+++ Module_3/Lec_1/req.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const fs = require('fs'); 
-let url = "http://www.google.com";
+const jsdom = require('jsdom');
+let url = "https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/kolkata-knight-riders-vs-lucknow-super-giants-66th-match-1304112/full-scorecard";
 
 // request working->
 //1. request to the given url;
@@ -13,17 +14,45 @@ request(url, cb);
 // body -> html
 console.log('Before');
 function cb(error, response, body) {
-    //   console.log('error:', error); // Print the error if one occurred
-    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //   console.log('body:', body); // Print the HTML for the Google homepage.
-    
     if (error) {
         console.log('error:', error.message); // Print the error message
     }else if(response && response.statusCode == 404){
         console.log("Page not found");
     }else {
-        console.log('body:',body);// Print the HTML for the Google homepage.
-        fs.writeFileSync("index.html",body);
+        // console.log('body:',body);// Print the HTML for the Google homepage.
+        // fs.writeFileSync("index.html",body);
+        console.log("content recieved");
+        extractData(body);
     }
 }
+
+function extractData(html) {
+    const JSDOM = jsdom.JSDOM;//no meaning
+    //1. string form me -> html content provide
+    // pass to newJSDOM
+    let dom = new JSDOM(html);
+    // 2. no meaning
+    let document = dom.window.document;
+    let firstButton = document.querySelectorAll(".lsb");
+    let content = firstButton[0].value;
+    console.log("content:",content);
+    let AllAnchor = document.querySelectorAll("#SIvCob a");
+    console.log("Google search is offered in these language");
+    for (let i = 0; i < AllAnchor.length; i++)
+    {
+        let content = AllAnchor[i].textContent;
+        console.log("languages:",content);
+    }
+    // console.log("Google search is offered in these language");
+    // for (let i = 0; i < AllAnchor2.length; i++)
+    // {
+        //     console.log("result:",content);
+        // }
+        let AllAnchor2 = document.querySelectorAll("#WqQANb a");
+        let Content = AllAnchor2[2].textContent;
+        console.log(Content);
+
+        
+}
+
 console.log('After'); 
