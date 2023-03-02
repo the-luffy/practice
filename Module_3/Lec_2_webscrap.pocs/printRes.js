6e0393fc58de964edf9310be5f6a59fc0f96507a
the-luffy <abhisheklaxkar007@gmail.com>
2023-03-02 18:00:23 +0530

    jsdom lib

---
+++ Module_3/Lec_2_webscrap.pocs/printRes.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const fs = require('fs'); 
+const jsdom = require('jsdom');
+let url = "https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/kolkata-knight-riders-vs-lucknow-super-giants-66th-match-1304112/full-scorecard";
+
+request(url, cb);
+console.log('Before');
+function cb(error, response, body) {
+    if (error) {
+        console.log('error:', error.message);
+    }else if(response && response.statusCode == 404) {
+        console.log("Page not found");
+    }else {
+        console.log("content recieved");
+        console.log(body);
+        extractData(body);
+    }
+}
+
+function extractData(html) {
+    const JSDOM = jsdom.JSDOM;
+    // pass to newJSDOM
+    let dom = new JSDOM(html);
+    // document represent the whole html page
+    let document = dom.window.document; 
+}
