ef29e62bf7021d3848be7697fabc7812f945cc35
the-luffy <abhisheklaxkar007@gmail.com>
2023-03-02 20:27:03 +0530

    added upto player

--- Module_3/Lec_2_webscrap.pocs/hwt.js
+++ Module_3/Lec_2_webscrap.pocs/hwt.js
@@ -24,7 +24,24 @@ function extractData(html) {
     // document represent the whole html page
     let document = dom.window.document;
     // using document and your selectors you find element in html page p span
-    let firstButton = document.querySelectorAll("p span");
-    let content = firstButton[8].textContent;
-    console.log(content);
+    let firstButton = document.querySelectorAll(".ds-w-full.ds-table.ds-table-md.ds-table-auto");
+    let content = firstButton[1]; // bowling table 2
+    let content2 = firstButton[3]; // bowling table 4
+    // element -> ke ander jo bhi html wo inner html se aa jayegi
+    let newHtmlString = "<table>" + content.innerHTML + "</table>" + "<table>" + content2.innerHTML + "</table>";
+    // fs.writeFileSync("tables.html", newHtmlString);
+    // console.log(newHtmlString);
+    getDataFromBowlingTable(newHtmlString);
+
+}
+
+function getDataFromBowlingTable(newHtmlString) {
+    const JSDOM = jsdom.JSDOM;
+    // pass to newJSDOM
+    let dom = new JSDOM(newHtmlString);
+    // document represent the whole html page
+    let document = dom.window.document;
+
+    let allRows = document.querySelectorAll("tbody td.ds-min-w-max.ds-flex.ds-items-center");
+    console.log(allRows.length);
 }
