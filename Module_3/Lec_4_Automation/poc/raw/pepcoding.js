25a6c01ef60c777f082aef48214c01fa53795623
the-luffy <abhisheklaxkar007@gmail.com>
2023-03-16 15:37:06 +0530

    added upto google search

--- Module_3/Lec_4_Automation/poc/raw/pepcoding.js
+++ Module_3/Lec_4_Automation/poc/raw/pepcoding.js
@@ -8,9 +8,14 @@ async function fn() {
     let browserRepresentativeObj = await puppeteer.launch({
         headless: false
     });
-    // tab 
+    // open new tab 
     let tab = await browserRepresentativeObj.newPage();
+    // to go google home page
     await tab.goto("https://www.google.com");
+    // type
+    await tab.type("input[type='text']","pepcoding", {delay : 200});
+    // to press a specific key
+    await tab.keyboard.press("Enter");
 
 }
 
