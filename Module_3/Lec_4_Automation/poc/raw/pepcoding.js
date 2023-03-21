2646f0ed13a727feecbb6d2beccfcf0fbc0dbba4
the-luffy <abhisheklaxkar007@gmail.com>
2023-03-21 21:53:53 +0530

    added pepcoding full app

--- Module_3/Lec_4_Automation/poc/raw/pepcoding.js
+++ Module_3/Lec_4_Automation/poc/raw/pepcoding.js
@@ -10,37 +10,63 @@ async function fn() {
         headless: false,
         executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
         defaultViewport: null,
-        arge: ["--start-maximized","--start-in-incognito"],
-        slowMo:100
+        arge: ["--start-maximized", "--start-in-incognito"],
+        slowMo: 100
     });
     // open new tab 
     let tab = await browserRepresentativeObj.newPage();
-    await tab.setViewport({ width: 0, height: 0});
+    await tab.setViewport({ width: 0, height: 0 });
     // to go google home page
     await tab.goto("https://www.google.com");
     // type text
-    await tab.type("input[type='text']","pepcoding", {delay : 200});
+    await tab.type("input[type='text']", "pepcoding", { delay: 200 });
     // to press a specific key
     await tab.keyboard.press("Enter");
     // page change error prevent -> to wait for selector tha tis present on the second page
-    await tab.waitForSelector(".LC20lb.MBeuO.DKV0Md", {visible: true});
+    await tab.waitForSelector(".LC20lb.MBeuO.DKV0Md", { visible: true });
     // click
-    await tab.click(".LC20lb.MBeuO.DKV0Md",{ delay: 200});
-    await tab.waitForSelector(".site-nav-li", {visible: true});
+    await tab.click(".LC20lb.MBeuO.DKV0Md", { delay: 200 });
+    await tab.waitForSelector(".site-nav-li", { visible: true });
     await tab.click(".site-nav-li");
-    await tab.waitForSelector(".row.fade.active .card", {visible: true });
+    await tab.waitForSelector(".row.fade.active", { visible: true });
 
     let courseNames = await tab.evaluate(fun);
     function fun() {
-        let elemArr = document.querySelectorAll(".card h3");
-        let courseNames = [];
-        for (let i = 0; i < elemArr.length; i++){
-            courseNames[i] = elemArr[i].textContent.trim();
+        let elemArr = document.querySelectorAll("#courses .card.course-tile.card-cs.rounded-border");
+        let detailArr = [];
+        for(let j = 0; j < elemArr.length; j++) {
+            let singleCard = elemArr[j];
+            let courseNameElem = singleCard.querySelector("h3");
+            let dateElem = singleCard.querySelector(".date");
+            let featureArr = singleCard.querySelectorAll("h5");
+    
+            let courseName = courseNameElem.textContent.trim();
+            let date = dateElem.textContent.trim();
+    
+            let feature = "";
+            for(let i = 0; i < featureArr.length; i++){
+                let featureElem = featureArr[i].textContent.trim();
+                feature += featureElem + "/n";
+            }
+            let priceArr = singleCard.querySelectorAll(".cart-sec h4");
+            // let price = priceArr[1].textContent.trim();
+            let price;
+            console.log(priceArr.length);
+            if(priceArr.length == 1) {
+                price = priceArr[0].textContent.trim();
+            } else {
+                price = priceArr[1].textContent.trim();
+            }
+            let courseObj = {
+                courseName, date, feature, price
+            }
+            detailArr.push(courseObj);
+
         }
-        return courseNames;
-        
+        return detailArr;
+
     }
-    console.log(courseNames);
+    console.table(courseNames);
 }
 
 
